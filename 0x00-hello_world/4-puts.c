5bb64dd3b0bed471b113e60b95eb11b70bfaf670
adekunle adeola <adekunleseun111@gmail.com>
2023-10-08 12:43:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
- * main - A program that prints a line withputs function
- * Return: 0 (pass)
- * /
+ * main - print "Programming is like building a multilingual puzzle,
+ * followed by a new line,"
+ * return 0.
+ */
+
 int main(void)
+
 {
- puts("\"Programming is like building a multilingual puzzle");
- return (0);
- } 
+	puts("\"Programming is like building a multilingual puzzle,);
+
+	return(0);
+}
