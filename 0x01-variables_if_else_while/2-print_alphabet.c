3a576497fdf1df414c64ea257c5482a22c18c141
adekunle adeola <adekunleseun111@gmail.com>
2023-10-09 14:37:13 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int main(void)
+	{
+		int i = 'a';
+		int k = 'A';
+
+while (i <= 'z') {
+	putchar (i);
+	i++;
+}
+while (k <= 'z') {
+        putchar (i);
+        k++;
+	putchar('\n');
+
+	return (0);
+}
