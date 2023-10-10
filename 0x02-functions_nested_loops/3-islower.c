921d5526a6c1e5ae4edb101aad23eeb0ee5373ee
adekunle adeola <adekunleseun111@gmail.com>
2023-10-10 15:03:43 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - a function that checks for lowercase character.
+ *
+ * @c: is a parameter
+ *
+ * Return: 0 or 1 
+ */
+
+int_islower(int c)
+{       
+        if(c >= 'a' && c <='z')
+		return (1);
+	else 
+		return (0);
+}
