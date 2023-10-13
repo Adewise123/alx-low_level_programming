40f06d43489bd3cb3bf8f15168842df3d7eb18fd
adekunle adeola <adekunleseun111@gmail.com>
2023-10-13 08:08:54 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Write a function that checks for uppercase character
+ * Returns 1 if c is uppercase
+ * Returns 0 otherwise
+ */
+
+int _issuper(int c) {
+        if (c >= 'A' && c <= 'Z')
+       	{
+                return 1;
+	}
+	else 
+	{
+		return 0;
+	}
+}
