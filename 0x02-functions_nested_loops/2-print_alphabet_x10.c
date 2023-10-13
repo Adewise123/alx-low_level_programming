44e098ddff14c3b6494ccdd1bdde6c96ec46d6ca
adekunle adeola <adekunleseun111@gmail.com>
2023-10-13 04:11:21 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - make alphabet x10 lines
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int i,j;
+
+	for (i = 0; i < 10; i++){
+		for (j = 'a'; j <= 'z'; j++){
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
